dab4440c60f1a8011762fbac476e8afa9a68fcb4
LY Heang <lyheang4248@gmail.com>
2023-06-23 23:33:16 +0700

    update: profile

--- app/src/main/java/kh/edu/rupp/ite/onlineshop/ui/fragment/ProfileFragment.java
+++ app/src/main/java/kh/edu/rupp/ite/onlineshop/ui/fragment/ProfileFragment.java
@@ -1,14 +1,20 @@
 package kh.edu.rupp.ite.onlineshop.ui.fragment;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
+
+import com.bumptech.glide.Glide;
 
 import kh.edu.rupp.ite.onlineshop.R;
 import kh.edu.rupp.ite.onlineshop.api.model.Profile;
@@ -22,8 +28,10 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class ProfileFragment extends Fragment {
 
     private EditText inputEmail, inputPhone, inputGender, inputBirthday, inputAddress;
-    private TextView email, first_name, last_name;
-
+    private TextView email, fullName;
+    private ImageView profilePicture;
+    private ProgressBar loadingSpinner;
+    private LinearLayout contentLayout, loadingLayout;
     public ProfileFragment() {
         // Required empty public constructor
     }
@@ -31,14 +39,18 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
-        first_name = view.findViewById(R.id.firstName);
-        last_name = view.findViewById(R.id.lastName);
+
+        fullName = view.findViewById(R.id.fullName);
         email = view.findViewById(R.id.email);
+        profilePicture = view.findViewById(R.id.profile_picture);
         inputEmail = view.findViewById(R.id.input_email);
         inputPhone = view.findViewById(R.id.input_phone);
         inputGender = view.findViewById(R.id.input_gender);
         inputBirthday = view.findViewById(R.id.input_birthday);
         inputAddress = view.findViewById(R.id.input_address);
+        loadingSpinner = view.findViewById(R.id.loading_spinner);
+        contentLayout = view.findViewById(R.id.content_layout);
+        loadingLayout = view.findViewById(R.id.loading_layout);
 
         loadProfileData();
 
@@ -46,33 +58,46 @@ public class ProfileFragment extends Fragment {
     }
 
     private void loadProfileData() {
-//        email.setText(profile.getEmail());
+        // Set it to visible
+        loadingSpinner.setVisibility(View.VISIBLE);
 
-        Retrofit retrofit = new Retrofit.Builder().baseUrl("https://raw.githubusercontent.com").addConverterFactory(GsonConverterFactory.create()).build();
-
-        ApiService apiService = retrofit.create(ApiService.class);
-        Call<Profile> call = apiService.getProfileData();
-
-        call.enqueue(new Callback<Profile>() {
+        new Handler().postDelayed(new Runnable() {
             @Override
-            public void onResponse(Call<Profile> call, Response<Profile> response) {
-                Profile profile = response.body();
-                if (profile != null) {
-                    first_name.setText(profile.getFirst_name());
-                    last_name.setText(profile.getLast_name());
-                    email.setText(profile.getEmail());
-                    inputEmail.setText(profile.getEmail());
-                    inputPhone.setText(profile.getPhoneNumber());
-                    inputGender.setText(profile.getGender());
-                    inputBirthday.setText(profile.getBirthday());
-                    inputAddress.setText(profile.getAddress());
-                }
-            }
+            public void run() {
+                Retrofit retrofit = new Retrofit.Builder().baseUrl("https://raw.githubusercontent.com").addConverterFactory(GsonConverterFactory.create()).build();
 
-            @Override
-            public void onFailure(Call<Profile> call, Throwable t) {
-                Toast.makeText(getActivity(), "Error loading profile data", Toast.LENGTH_SHORT).show();
+                ApiService apiService = retrofit.create(ApiService.class);
+                Call<Profile> call = apiService.getProfileData();
+
+                call.enqueue(new Callback<Profile>() {
+                    @Override
+                    public void onResponse(Call<Profile> call, Response<Profile> response) {
+                        Profile profile = response.body();
+                        if (profile != null) {
+                            fullName.setText(profile.getFirst_name() + " " + profile.getLast_name());
+                            email.setText(profile.getEmail());
+                            inputEmail.setText(profile.getEmail());
+                            inputPhone.setText(profile.getPhoneNumber());
+                            inputGender.setText(profile.getGender());
+                            inputBirthday.setText(profile.getBirthday());
+                            inputAddress.setText(profile.getAddress());
+                            // Load profile image with Glide
+                            Glide.with(getActivity()).load(profile.getImageUrl()).placeholder(R.drawable.profile).circleCrop()  // This line makes the image rounded
+                                    .into(profilePicture);
+                        }
+                        // After loading data, set the ProgressBar to gone
+                        loadingLayout.setVisibility(View.GONE);
+                        contentLayout.setVisibility(View.VISIBLE);
+                    }
+
+                    @Override
+                    public void onFailure(Call<Profile> call, Throwable t) {
+                        Toast.makeText(getActivity(), "Error loading profile data", Toast.LENGTH_SHORT).show();
+                        // In case of failure too, set the ProgressBar to gone
+                        loadingSpinner.setVisibility(View.GONE);
+                    }
+                });
             }
-        });
+        }, 1000);  // This 3000 here is the delay time in milliseconds. This means the code inside run() will execute after 3 seconds.
     }
 }
